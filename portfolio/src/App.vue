3f4d1cda5a67440adb08a0e00ad5af6a83ce6fff
DThaiPome <r.gehrlein154@gmail.com>
2021-07-31 21:03:32 -0400

    Made a cute header with palceholder links

--- portfolio/src/App.vue
+++ portfolio/src/App.vue
@@ -1,28 +1,36 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!--img alt="Vue logo" src="./assets/logo.png"-->
+    <global-header/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import GlobalHeader from './components/GlobalHeader.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    GlobalHeader
   }
 }
 </script>
 
-<style>
+<style lang='less'>
+@import './assets/style/colors';
+
+body {
+  background-color: @BG-COLOR-2;
+  color: @TEXT-COLOR;
+  margin: 0pt;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
+  /* -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 60px; */
 }
 </style>
---
+++ portfolio/src/components/GlobalHeader.vue
@@ -0,0 +1,75 @@
+<template>
+    <div class="header-container">
+            <div class="header-title">Ryan Gehrlein</div>
+            <div class="subheader-title">Game Programmer &amp; Software Engineer</div>
+            <hr noshade>
+            <div class="links-container">
+                <span class="link-box" v-for="(button, idx) in buttons" :key="idx">
+                    <span class="link-text" v-text="button"/>
+                    <span class="vertical-rule" v-if="idx != buttons.length - 1"/>
+                </span>
+            </div>
+        </div>
+</template>
+
+<script>
+export default {
+    props: {
+        pageName: {
+            type: String,
+            default: ''
+        }
+    },
+    computed: {
+        buttons: () => {
+            return [
+                'Home',
+                'About',
+                'Resume',
+                'Contact',
+                'Featured Code'
+            ];
+        }
+    }
+}
+</script>
+
+<style lang='less' scoped>
+    @import '../assets/style/colors';
+
+    .header-container {
+        text-align: center;
+        background-color: @BG-COLOR;
+        border: 4pt solid @BORDER-COLOR;
+        border-radius: 40pt;
+        width: auto;
+        margin: auto;
+        padding: 10pt;
+
+        .header-title {
+            font-weight: bolder;
+            font-size: 60px;
+        }
+
+        .subheader-title {
+            font-size: 24px;
+        }
+
+        hr {
+            color: @BORDER-COLOR;
+        }
+        
+        .links-container {
+            .link-box {
+                .link-text {
+                    margin: 10pt;
+                }
+
+                .vertical-rule {
+                    border: 1pt solid @BORDER-COLOR;
+                    height: 40pt;
+                }
+            }
+        }
+    }
+</style>
