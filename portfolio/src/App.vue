9d3f258bf4bfdf97f923d0ca96d3aa398720bc70
DThaiPome <r.gehrlein154@gmail.com>
2021-08-01 00:00:42 -0400

    The background moves this is wonderful

--- portfolio/src/App.vue
+++ portfolio/src/App.vue
@@ -18,6 +18,7 @@ export default {
 
 <style lang='less'>
 @import './assets/style/colors';
+@import './assets/style/animations';
 
 body {
   background-image: url('./assets/trippy background-small.png'), 
@@ -26,14 +27,31 @@ body {
   background-blend-mode: color;
   color: @TEXT-COLOR;
   margin: 0pt;
+
+  animation-duration: sqrt(pow(@INFINITE-TIME, 2) + pow(@INFINITE-TIME, 2)) * 1s * @INFINITE-TIME-SPEED-CO;
+  animation-name: move;
+  animation-iteration-count: infinite;
+  animation-timing-function: linear;
 }
 
 #app {
+  backdrop-filter: blur(100%);
   font-family: Avenir, Helvetica, Arial, sans-serif;
+  width: 100%;
+  height: 100%;
   /* -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px; */
+}
+
+@keyframes move {
+  0% {
+    background-position: 0pt 0pt;
+  }
+  100% {
+    background-position: @INFINITE-TIME * 1pt @INFINITE-TIME * 1pt;
+  }
 }
 </style>
